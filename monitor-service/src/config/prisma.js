3642ae26279f64efcdd5cc4fbf13cf04e4339fe5
Aryan <vedprakashverma4842@gmail.com>
2026-03-29 14:27:16 +0530

    restore database URL in datasource for Prisma v7

--- monitor-service/src/config/prisma.js
+++ monitor-service/src/config/prisma.js
@@ -2,12 +2,6 @@ import pkg from "@prisma/client";
 
 const { PrismaClient } = pkg;
 
-// For Prisma v7+, datasource is configured via DATABASE_URL environment variable
 export const prisma = new PrismaClient({
-  log: [
-    {
-      emit: "stdout",
-      level: "error",
-    },
-  ],
+  log: ["error"],
 });
